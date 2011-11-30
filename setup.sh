3051083ad801beeb459bdba9af1ebfbfd6f94314
Prod04 <arpu@codeinvain.com>
2011-11-30 22:25:27 +0400

    Added ssmtp config

--- setup.sh
+++ setup.sh
@@ -3,20 +3,15 @@
 
 echo "Starting server configuration script..."
 
-# Install Monit
-rm /etc/default/monit
-ln /opt/arpu_lift/server-config/monit/monit
-
-rm /etc/monit/conf.d
-ln /opt/arpu_lift/server-config/monit/conf.d /etc/monit/conf.d -s
-
-# Install nginx
+# Setup nginx
+echo "Setup nginx"
 rm /opt/nginx/conf/nginx.conf
 ln /opt/arpu_lift/server-config/nginx/nginx.conf /opt/nginx/conf/nginx.conf -s
 ln /opt/arpu_lift/server-config/nginx/htpasswrd  /opt/nginx/conf/htpasswd -s
 ln /opt/arpu_lift/dev_tools/public /opt/arpu_lift/dev_tools/dev -s
 
 # Setup mysql
+echo "Setup mysql"
 rm /etc/mysql/my.cnf
 ln /opt/arpu_lift/server-config/mysql/my.cnf /etc/mysql/my.cnf -s
 echo -n "Enter replication ID for mysql: "
@@ -24,5 +19,21 @@ read repid
 sed -i "s/SERVERID/$repid/g" /opt/arpu_lift/server-config/mysql/my.cnf
 echo "Updated mysql conf with id: $repid"
 
+# Setup ssmtp
+echo "Setup ssmtp"
+rm /etc/ssmtp/ssmtp.conf 
+ln /opt/arpu_lift/server-config/ssmtp/ssmtp.conf /etc/ssmtp/ssmtp.conf -s
+sed -i "s/MYHOSTNAME/`hostname`/g" /opt/arpu_lift/server-config/ssmtp/ssmtp.conf
+
+# Setup Monit
+echo "Setup monit"
+rm /etc/default/monit
+ln /opt/arpu_lift/server-config/monit/monit
+
+rm /etc/monit/conf.d
+ln /opt/arpu_lift/server-config/monit/conf.d /etc/monit/conf.d -s
+
+monit monitor all
+monit restart all
 
 echo "Finished server configuration"
