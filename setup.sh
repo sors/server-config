8042bff3580b65ec36bca28cebe0509f9c460686
root <root@arpu-lift04>
2011-11-30 17:11:12 +0400

    Added basic monit scripts

--- setup.sh
+++ setup.sh
@@ -1,2 +1,9 @@
 #! /bin/sh
 # write here the copy / link script
+
+# Install Monit
+rm /etc/default/monit
+ln /opt/arpu_lift/server-config/monit/monit
+
+rm /etc/monit/conf.d
+ln /opt/arpu_lift/server-config/monit/conf.d /etc/monit/conf.d -s
