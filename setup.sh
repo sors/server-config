3c7ed35949c3c74b8cee6bc24e8aba70ca36728a
Prod04 <arpu@codeinvain.com>
2011-11-30 18:13:19 +0400

    Added basic nginx config

--- setup.sh
+++ setup.sh
@@ -7,3 +7,11 @@ ln /opt/arpu_lift/server-config/monit/monit
 
 rm /etc/monit/conf.d
 ln /opt/arpu_lift/server-config/monit/conf.d /etc/monit/conf.d -s
+
+# Install nginx
+rm /opt/nginx/conf/nginx.conf
+ln /opt/arpu_lift/server-config/nginx/nginx.conf /opt/nginx/conf/nginx.conf -s
+ln /opt/arpu_lift/server-config/nginx/htpasswrd  /opt/nginx/conf/htpasswd -s
+ln /opt/arpu_lift/dev_tools/public /opt/arpu_lift/dev_tools/dev -s
+
+
