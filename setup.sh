ca65c2025fce6f22d879c87f939d4db4c0a4bd71
Prod04 <arpu@codeinvain.com>
2011-11-30 21:10:08 +0400

    Added mysql conf script

--- setup.sh
+++ setup.sh
@@ -1,6 +1,8 @@
 #! /bin/sh
 # write here the copy / link script
 
+echo "Starting server configuration script..."
+
 # Install Monit
 rm /etc/default/monit
 ln /opt/arpu_lift/server-config/monit/monit
@@ -14,4 +16,13 @@ ln /opt/arpu_lift/server-config/nginx/nginx.conf /opt/nginx/conf/nginx.conf -s
 ln /opt/arpu_lift/server-config/nginx/htpasswrd  /opt/nginx/conf/htpasswd -s
 ln /opt/arpu_lift/dev_tools/public /opt/arpu_lift/dev_tools/dev -s
 
+# Setup mysql
+rm /etc/mysql/my.cnf
+ln /opt/arpu_lift/server-config/mysql/my.cnf /etc/mysql/my.cnf -s
+echo -n "Enter replication ID for mysql: "
+read repid
+sed -i "s/SERVERID/$repid/g" /opt/arpu_lift/server-config/mysql/my.cnf
+echo "Updated mysql conf with id: $repid"
 
+
+echo "Finished server configuration"
